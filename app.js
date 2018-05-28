3900a9e47596ccc55bac60f2428a95bbefaec6c4
Mohamed BOUHAMED <med.bouhamed@live.fr>
2018-05-28 14:28:44 +0200

    A simple synchrone program

---
+++ app.js
@@ -0,0 +1,8 @@
+//here is a simple exemple about the synchronous programming
+//we have to print one then tow and in the end three
+//so it's like never gonna be print in the console tow before one
+// or three before tow and one  
+console.log ('one');
+console.log ('tow');
+console.log ('three');
+
