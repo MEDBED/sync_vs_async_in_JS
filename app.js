cf46bbaa11438802c927db5c2213a608da14d46e
Mohamed BOUHAMED <med.bouhamed@live.fr>
2018-05-28 14:53:33 +0200

    ASynchronous programming

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-//here is a simple exemple about the synchronous programming problems
-//where there is an instruction that takes a lot of time!
-function longRun() {
-    var x = 0; 
-    while(x < 10000000000){
-        x++;
-    }
-    console.log('Done !');    
+//here is a simple exemple about an Asynchronous programming
+//where there is an instruction that takes a lot of time one of the solutions is to use 
+//the setTimeOut Function to do the instructions then comeback for it.
+function longRun(callBack) {
+    setTimeout(() => {
+        var x = 0; 
+        while(x < 10000000000){
+            x++;
+        }
+        callBack("All Done !");
+    }, 0);    
 }
 
 console.log (new Date().getTime());
 //so here the next instruction will takes a lot of time
-longRun();
+longRun((msg) => {console.log(msg)});
 console.log (new Date().getTime());
