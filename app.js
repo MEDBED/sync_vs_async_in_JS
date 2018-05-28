fc66803d37cf6e49328879c45980b08f977defaf
Mohamed BOUHAMED <med.bouhamed@live.fr>
2018-05-28 16:03:34 +0200

    ASynchronous programming

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function longRun(callBack) {
         while(x < 10000000000){
             x++;
         }
-        callBack("All Done !");
+        callBack("All Done First Exemple !");
     }, 0);    
 }
 
@@ -16,3 +16,35 @@ console.log (new Date().getTime());
 //After that we run the next instruction
 longRun((msg) => {console.log(msg)});
 console.log (new Date().getTime());
+
+//---------- setTimeout ------------------
+let y = 100;
+//Async instruction
+setTimeout(() => {
+    y++;
+    console.log('All Done second example !');
+}, 0);
+// simple log
+console.log(y);
+//Async instruction
+setTimeout(() => {
+    console.log(y);
+    console.log('All Done second example !');
+});
+
+//------------Promises--------------------
+let promise = new Promise((resolve,reject) => {
+    setTimeout(() =>{
+        resolve("yo !");
+    },0);
+});
+console.log(promise);
+//this one will wait 10 ms for the promise then wil execute the console
+//and this can not be a good solution because you don't know when you will receive the promise.
+setTimeout(() => {
+    console.log(promise);
+},10);
+//in order to wait for the data and then execute the promise
+promise.then((val) => {
+    console.log(val);
+})
